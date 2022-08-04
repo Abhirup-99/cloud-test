a9b6917e41d3cf26aa6053dbe7ae3e8819bf08fd
Abhirup <abhirup@even.in>
2022-08-05 02:39:00 +0530

    Better solution.

--- src/state/workloads/epics.ts
+++ src/state/workloads/epics.ts
@@ -39,18 +39,39 @@ export const cancelWorkload: AppEpic = (action$, state$) => (
   )
 );
 
-export const checkWorkloadStatus: AppEpic = (action$, state$) => (
+/**
+ * the trick was to add an additional delay in the second timer
+ * which would ensure that the 2nd timer would always finish at
+ * the end. We can make optimisations on how much you can reduce
+ * the delay, but due to the nature of JS being a single thread
+ * and the presence of 2 different queues for setTimeouts and
+ * promises, you can never Deterministically say which one would
+ * get executed. A great article read on rxjs schedulers on which
+ * the delay function was based on:
+ * https://blog.strongbrew.io/what-are-schedulers-in-rxjs/
+ * delay internals:
+ * https://github.com/ReactiveX/rxjs/blob/c45e07cef887fded09473ce64ea59f54a77e8efc/src/internal/operators/delay.ts
+ */
+export const checkWorkloadStatus: AppEpic = (action$, state$) =>
   action$.pipe(
     filter(isActionOf(workloadsActions.created)),
-    mergeMap(action => from(workloadService.checkStatus(action.payload)).pipe(
-      delay(action.payload.completeDate),
-      delay(1000),
-      tap(response => console.log('workload status check', response)),
-      map(response => workloadsActions.updateStatus(response)),
-      takeUntil(action$.pipe(
-        filter(isActionOf(workloadsActions.cancel)),
-      ))
-    )),
+    mergeMap((action) =>
+      from(workloadService.checkStatus(action.payload)).pipe(
+        map((response) => ({ ...response })),
+        delay(action.payload.completeDate),
+        delay(10),
+        mergeMap((response) =>
+          from(workloadService.checkStatus({ id: response.id })).pipe(
+            map((response) =>
+              workloadsActions.updateStatus({
+                id: response.id,
+                status: response.status,
+              })
+            )
+        )
+      ),
+      takeUntil(action$.pipe(filter(isActionOf(workloadsActions.cancel))))
+    )
   )
 );
 
